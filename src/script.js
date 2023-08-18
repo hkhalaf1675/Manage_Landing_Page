e628af7c53cccfea0b0c7390c542671eca2db239
Hassan Tawfik <h.khalaf16.75@gmail.com>
2023-08-18 04:08:30 +0200

    new tailwindcss project

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let header = document.getElementById("header")
+
+window.addEventListener("scroll",() => {
+    if(window.scrollY > 100){
+        header.style.backgroundColor = "#fafafa";
+        header.style.padding = "1rem 4rem";
+    }
+    else{
+        header.style.backgroundColor = "transparent";
+        header.style.padding = "2rem 4rem";
+    }
+})
